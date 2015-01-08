13366be6d2c164b9d10cba7d9f04493280e80ef0
Bruce LANE <bruce@batchass.fr>
2015-01-08 19:18:42 +0100

    using reymenta block

--- src/ReymentaHapPlayerApp.cpp
+++ src/ReymentaHapPlayerApp.cpp
@@ -1,33 +1,33 @@
 /*
 
-		Basic Spout app for Cinder
+Basic Spout app for Cinder
 
-		Search for "SPOUT" to see what is required
-		Uses the Spout dll
+Search for "SPOUT" to see what is required
+Uses the Spout dll
 
-		Based on the RotatingBox CINDER example without much modification
-		Nothing fancy about this, just the basics.
+Based on the RotatingBox CINDER example without much modification
+Nothing fancy about this, just the basics.
 
-		Search for "SPOUT" to see what is required
+Search for "SPOUT" to see what is required
 
-		==========================================================================
-		Copyright (C) 2014 Lynn Jarvis.
+==========================================================================
+Copyright (C) 2014 Lynn Jarvis.
 
-		This program is free software: you can redistribute it and/or modify
-		it under the terms of the GNU Lesser General Public License as published by
-		the Free Software Foundation, either version 3 of the License, or
-		(at your option) any later version.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-		This program is distributed in the hope that it will be useful,
-		but WITHOUT ANY WARRANTY; without even the implied warranty of
-		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-		GNU Lesser General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
 
-		You should have received a copy of the GNU Lesser General Public License
-		along with this program.  If not, see <http://www.gnu.org/licenses/>.
-		==========================================================================
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+==========================================================================
 
-		*/
+*/
 
 #include "cinder/app/AppNative.h"
 #include "cinder/app/RendererGl.h"
